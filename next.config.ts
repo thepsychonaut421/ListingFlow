f6d61047ce06e66251ba45ec212214e920ef8150
Rares Balauta <rares.balauta19@gmail.com>
2025-08-01 16:28:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'shop.retoura.de',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
   devIndicators: {
