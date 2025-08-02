5f94770933c91b7087ac2db96041eab2281fac1c
Rares Balauta <rares.balauta19@gmail.com>
2025-08-02 18:08:28 +0000

    alt fail;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -464,7 +464,7 @@ function DashboardClient() {
               <SheetTrigger asChild>
                   <Button size="sm" className="h-8 gap-1" onClick={handleAddProduct}>
                   <PlusCircle className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-rap">
+                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
                     Add Product
                   </span>
                 </Button>
